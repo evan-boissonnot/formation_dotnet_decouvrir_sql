c3ef6ed62b617a78b2f70e681baa7a6426965c5b
evan-boissonnot <commercial@boissonnot.Fr>
2018-11-23 18:40:26 +0100

    mise à jour avec découverte entitiesé

---
+++ DecouverteEntities/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecouverteEntities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (KartinaEntities context = new KartinaEntities())
+            {
+                var listVendeurRequete = from vend in context.Vendeur
+                                         select vend;
+
+                var maListeDeVendeurs = listVendeurRequete.ToList();
+
+                foreach (var vendeur in maListeDeVendeurs)
+                {
+                    Console.WriteLine(vendeur.FirstName + ", " + vendeur.LastName);
+                }
+            }
+        }
+    }
+}
